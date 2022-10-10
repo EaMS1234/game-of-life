9ab579b6259f4b780e084c37fc25ea4536878d16
Erick Augusto <52828716+EaMS1234@users.noreply.github.com>
2022-10-10 12:02:26 -0300

    Updated index.html and index.js

--- scripts/index.js
+++ scripts/index.js
@@ -1,14 +1,17 @@
-// Code that control's the game's page
+// Code that handles the game's page
 
 // Variables
 var playing = false
+var width = Math.floor(window.innerWidth / 20) * 20 - 100
+var height = Math.floor(window.innerHeight / 20) * 20 - 200
+var game = {}
 
 // HTML's elements
 const playPauseButton = document.getElementById('play_pause_button')
 const generateButton = document.getElementById('generate_new_game')
 const generationsPersec = document.getElementById('generations_per_sec')
 
-// Play-pause button pressed
+// "Play/pause" button handler
 function playPause() {
     playing = !playing
 
@@ -19,16 +22,25 @@ function playPause() {
     }
 }
 
-function main() {
-    // Configures the canvas
-    let width = Math.floor(window.innerWidth / 5) * 5 - 100
-    let height = Math.floor(window.innerHeight / 5) * 5 - 200
+// "Generate" button handler
+function newGame(random = true) {
+    // Re-generates a game
+    game = generateWorld((height / 20), (width / 20), random)
+}
 
+// Configures the page
+window.onload = function () {
+    // Generates a new game
+    newGame()
+
+    // Configures the canvas
     const canvas = document.getElementById('game_board')
     canvas.setAttribute('width', `${width}`)
     canvas.setAttribute('height', `${height}`)
 
     const ctx = canvas.getContext('2d')
-}
 
-main()
+    for (let i = 0; i < game.length; i++) {
+        let currentCell = game[i];
+    }
+}
